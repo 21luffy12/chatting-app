7361392f7981d149ae6301682d368892e6338c78
21luffy12 <103305081+21luffy12@users.noreply.github.com>
2024-11-21 18:44:13 +0530

    Delete src directory

--- src/zustand/useConverstaion.js
+++
@@ -1,9 +0,0 @@
-import {create} from "zustand"
-const useConverstaion = create((set) => ({
-  selectedConversation: null,
-  setSelectedConversation: (selectedConversation) => set({selectedConversation}),
-  messages : [],
-  setMessages: (messages) => set({messages}),
-}))
-
-export default useConverstaion;
